a12ec6aeca374d968423f6ddb74d39df9a069efb
Travis <tscholl2@uw.edu>
2017-08-20 13:23:46 -0700

    example demo switch

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,13 @@ import { h, app } from "hyperapp";
 import { mixins } from "./mixins";
 import { State, initialState } from "./state";
 import { actions, Actions } from "./actions";
+import { NewRouter } from "./components";
+
+const router = NewRouter([
+  { route: "/", component: () => h("p", undefined, "home") },
+  { route: "/:id", component: ({ params: { id = "" } }) => h("p", undefined, `id = ${id}`) },
+  { route: "*", component: () => h("p", undefined, "not found") },
+]);
 
 export default (state = initialState) => {
   app<State, Actions, { route: any }>({
@@ -12,7 +19,8 @@ export default (state = initialState) => {
         "div",
         undefined,
         h("h1", undefined, state.title),
-        h("button", { onclick: () => actions.router.go("p1") }, "hi"),
+        h("button", { onclick: () => actions.router.go("/p1") }, "hi"),
+        router(state.path),
       ),
     mixins,
   });
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,8 @@ import { Component } from "hyperapp";
 import { routesMatcher, Params } from "../utils";
 
 export function NewRouter(
-  routes: Array<{ route: string; component: Component<{ params: Params }> }>,
-): Component<string, { params: Params }> {
+  routes: Array<{ route: string; component: Component<{ params: Params }, any> }>,
+): Component<string, any> {
   const rm = routesMatcher(routes.map(r => r.route));
   return (path, ...children) => {
     const match = rm(path);
