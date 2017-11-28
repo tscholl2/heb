13e3078d1ba3c0fcad69468f179c6dda117d9928
Travis Scholl <tscholl2@uw.edu>
2017-11-28 09:58:15 -0800

    lint

--- src/components/calculator/index.ts
+++ src/components/calculator/index.ts
@@ -9,11 +9,7 @@ export interface ICalculatorProps {
 
 export function Calculator({ value = "", update }: ICalculatorProps) {
   const B = (c: string, f?: any) =>
-    h(
-      "button",
-      { class: "calculator-button", onclick: f ? f : () => update(value + c) },
-      c,
-    );
+    h("button", { class: "calculator-button", onclick: f ? f : () => update(value + c) }, c);
   const R = (children: any) => h("div", { class: "calculator-buttons-row" }, children);
   return h("div", { class: "calculator-container" }, [
     h("code", { class: "calculator-screen" }, value),
@@ -21,12 +17,7 @@ export function Calculator({ value = "", update }: ICalculatorProps) {
       R([B("0"), B("1"), B("2"), B("3")]),
       R([B("4"), B("5"), B("6"), B("7")]),
       R([B("8"), B("9"), B("+"), B("-")]),
-      R([
-        B("*"),
-        B("/"),
-        B("clear", () => update("")),
-        B("=", () => update(`${eval(value)}`)),
-      ]),
+      R([B("*"), B("/"), B("clear", () => update("")), B("=", () => update(`${eval(value)}`))]),
     ]),
   ]);
 }
