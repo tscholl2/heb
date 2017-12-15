3c851eab1ad36603e98b596945b5ecd866d30bc3
Travis Scholl <tscholl2@uw.edu>
2017-12-14 22:52:32 -0800

    remove old body children to help with hot reloading

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { initialState, IState } from "./model";
 import { addListener } from "./addons/router";
 
 function run() {
+  document.body.removeChild(document.body.childNodes.item(0));
   const controller = new Controller<IState>();
   let node: any;
   const v = view(controller.dispatch);
