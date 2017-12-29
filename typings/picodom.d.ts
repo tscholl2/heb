da53a5c28326a66e4e79efcc0ed804d7c648836d
Travis Scholl <tscholl2@uw.edu>
2017-12-29 09:38:21 -0800

    added lifecycle events, need to add these into vnode somehow

--- typings/picodom.d.ts
+++ typings/picodom.d.ts
@@ -5,17 +5,28 @@ declare module "picodom" {
     children: Array<VNode<{}> | string>;
   }
 
-  export interface Component<Props = {}> {
-    (props: Props, children?: Array<VNode<{}> | string>): VNode<Props>;
+  export interface LifeCycleEvents<Props = any> {
+    /**
+     * Fired after the element is created and attached to the DOM.
+     */
+    oncreate?(el: HTMLElement): void;
+    /**
+     * Fired after the element attributes are updated.
+     * This event will fire even if the attributes have not changed.
+     */
+    onupdate?(el: HTMLElement, oldProps: Props): void;
+    /**
+     * Fired before the element is removed from the DOM.
+     * Return a function that takes a remove() function and use it to remove the element asynchronously.
+     */
+    onremove?(el: HTMLElement): void | ((remove: () => void) => void);
   }
 
-  export function h<Props extends {}>(
-    tag: Component<Props> | string,
-    props: Props,
-    children?: Array<VNode | string | number | null | undefined>,
-  ): VNode<Props>;
+  export interface Component<Props = {}> {
+    (props: Props, children?: Array<VNode | string>): VNode;
+  }
 
-  export function h<Props>(
+  export function h<Props = {}>(
     tag: Component<Props> | string,
     props?: Props,
     children?: Array<VNode | string | number | null | undefined>,
