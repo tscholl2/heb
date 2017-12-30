ef8e02e133a36d5457538fecdcad980cfd6ed1ee
Travis <tscholl2@uw.edu>
2017-12-29 16:44:24 -0800

    another try at web components, works in chrome

--- src/view/components/markdown/index2.ts
+++ src/view/components/markdown/index2.ts
@@ -1,29 +1,69 @@
 import { h, Component, patch, VNode } from "picodom";
 
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function `fn` will be called after it stops being called for
+// `N` milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(N: number, fn: (...args: any[]) => void, immediate = false) {
+  let timeout: any;
+  return function(...args: any[]) {
+    const later = function() {
+      timeout = undefined;
+      if (!immediate) fn(...args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, N);
+    if (callNow) fn(...args);
+  };
+}
+
+function renderMarkdown(markdown: string) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(h("p", undefined, [`done: ${markdown}`]));
+    }, 2000);
+  });
+}
+
 class XMarkdown extends HTMLElement {
-  private node: VNode<any>;
-  static get observedAttributes() {
+  private node: VNode;
+  private renderedMarkdown: VNode<any> | undefined;
+  private render: any;
+  public static get observedAttributes() {
     return ["markdown"];
   }
+  private loading = h("progress");
   constructor() {
     super();
-    // try {super()} catch (e) {}
+    this.render = debounce(16, () => {
+      console.log("rendering");
+      const newNode = this.renderedMarkdown || this.loading;
+      console.log(newNode);
+      console.log(this.node);
+      if (newNode !== this.node) {
+        patch(this.node, newNode, this);
+        this.node = newNode;
+      }
+    });
+  }
+  private update(newValue: string) {
+    this.renderedMarkdown = undefined;
+    renderMarkdown(newValue).then((value: any) => {
+      this.renderedMarkdown = value;
+      this.render();
+    });
+    this.render();
   }
   attributeChangedCallback(attr: any, oldValue: any, newValue: any) {
     if (attr === "markdown") {
-      this.textContent = `Hello, ${newValue}`;
+      console.log(`value changed from ${oldValue} to ${newValue}`);
+      this.update(newValue);
     }
   }
   connectedCallback() {
-    console.log("my attribute is ", this.getAttribute("markdown"));
-    const newNode = h("progress");
-    patch(this.node, newNode, this);
-    this.node = newNode;
-    setTimeout(() => {
-      const newNode = h("div", undefined, [`done: ${this.getAttribute("markdown") || ""}`]);
-      patch(this.node, newNode, this);
-      this.node = newNode;
-    }, 2000);
+    console.log("connected to dom");
+    this.update(this.getAttribute("markdown") || "");
   }
   disconnectedCallback() {
     console.log("leaving dom!");
--- src/view/components/markdown/markdown_sample.ts
+++ src/view/components/markdown/markdown_sample.ts
@@ -1,19 +1,24 @@
 import { addStatefullSample } from "../../../samples";
 // import { Markdown } from "./";
-const { Markdown } = require("./index3.ts");
+const { Markdown } = require("./index2.ts");
 import { h } from "picodom";
 
-addStatefullSample<{ value: string }>("markdown", (s = { value: "" }, dispatch) =>
-  h("div", undefined, [
-    h(
-      "button",
-      {
-        style: { width: "200px", height: "200px" },
-        onclick: () => dispatch(() => ({ value: s.value === "1+1" ? "2+2" : "1+1" })),
-      },
-      ["toggle"],
-    ),
-    h("br"),
-    Markdown({ markdown: s.value, update: () => dispatch(s => Object.assign({}, s)) }),
-  ]),
+addStatefullSample<{ value: string; fake: number }>(
+  "markdown",
+  (s = { value: "", fake: 0 }, dispatch) =>
+    h("div", undefined, [
+      h("h1", undefined, [s.fake]),
+      h("button", { onclick: () => dispatch(() => ({ ...s, fake: s.fake + 1 })) }, ["counter"]),
+      h("br"),
+      h(
+        "button",
+        {
+          style: { width: "200px", height: "200px" },
+          onclick: () => dispatch(() => ({ ...s, value: s.value === "1+1" ? "2+2" : "1+1" })),
+        },
+        ["toggle"],
+      ),
+      h("br"),
+      Markdown({ markdown: s.value, update: () => dispatch(s => Object.assign({}, s)) }),
+    ]),
 );
